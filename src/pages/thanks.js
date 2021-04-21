d63f7679caa2b54e03ac835a4958a4d86cebaf11
Audrey Wong <audreydotwong@gmail.com>
2021-04-21 15:52:59 -0400

    edit thank you page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,5 +1,28 @@
 import * as React from "react"
 
-export default () => (
-    <h1>Thank you for your form submissions!</h1>
-)
+// styles
+const pageStyles = {
+    color: "#232129",
+    padding: 96,
+    fontFamily: "-apple-system, Roboto, sans-serif, serif"
+}
+const headingStyles = {
+    marginTop: 0,
+    marginBottom: 64,
+    maxWidth: 320
+}
+const headingAccentStyles = {
+    color: "#663399"
+}
+
+export default() => (<main style={pageStyles}>
+    <h1 style={headingStyles}>
+        Congratulations
+        <br/>
+        <span style={headingAccentStyles}>— you just submitted your form!
+        </span>
+        <span role="img" aria-label="Party popper emojis">
+            🎉🎉🎉
+        </span>
+    </h1>
+</main>)
